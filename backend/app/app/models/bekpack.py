cea9543b172236e725263f48f2a7ab8090d84f2f
Christopher Chen <christopher.chen1995@gmail.com>
2020-12-22 15:54:41 -0800

    In BekPack models fix and sort column names

--- backend/app/app/models/bekpack.py
+++ backend/app/app/models/bekpack.py
@@ -12,68 +12,68 @@ if TYPE_CHECKING:
 
 
 class BekPackTrip_Members(Base):
-    user_id = Column(
-        Integer, ForeignKey("bekpackuser.id"), primary_key=True, index=True
-    )
     trip_id = Column(
         Integer, ForeignKey("bekpacktrip.id"), primary_key=True, index=True
+    )
+    user_id = Column(
+        Integer, ForeignKey("bekpackuser.id"), primary_key=True, index=True
     )
 
 
 class BekpackUser(Base):
     id = Column(Integer, primary_key=True, index=True)
-    owner_id = Column(Integer, ForeignKey("user.id"))
     is_active = Column(Boolean(), default=True)
     joined_trips = relationship(
         "BekpackTrip",
         secondary=BekPackTrip_Members,
         back_populates="members",
     )
-    owned_trips = relationship("BekpackTrip", back_populates="owner")
     owned_bags = relationship("BekpackBag", back_populates="owner")
+    owned_trips = relationship("BekpackTrip", back_populates="owner")
+    owner_id = Column(Integer, ForeignKey("user.id"))
 
 
 class BekpackTrip(Base):
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    is_active = Column(Boolean(), default=True)
-    owner_id = Column(Integer, ForeignKey(BekpackUser.id))
+    bags = relationship("BekpackBag", back_populates="owner_list")
     color = Column(ColorType)
-    owner = relationship(BekpackUser, back_populates=BekpackUser.owned_trips)
+    is_active = Column(Boolean(), default=True)
     members = relationship(
         BekpackUser,
         secondary=BekPackTrip_Members,
         back_populates=BekpackUser.joined_trips,
     )
-    bags = relationship("BekpackBag", back_populates="owner_list")
+    name = Column(String, index=True)
+    owner = relationship(BekpackUser, back_populates=BekpackUser.owned_trips)
+    owner_id = Column(Integer, ForeignKey(BekpackUser.id))
 
 
 class BekpackTripItemList(Base):
     id = Column(Integer, primary_key=True, index=True)
-    parent_trip = Column(Integer, ForeignKey(BekpackTrip.id), primary_key=True)
-    parent_user = Column(Integer, ForeignKey(BekpackUser.id), primary_key=True)
     name = Column(String)
+    parent_trip_id = Column(Integer, ForeignKey(BekpackTrip.id), primary_key=True)
+    parent_user_id = Column(Integer, ForeignKey(BekpackUser.id), primary_key=True)
 
 
 class BekpackBag(Base):
-    id = Column(Integer, primary_key=True, index=True)
-
-    owner_id = Column(Integer, ForeignKey(BekpackUser.id), index=True)
-    owner = relationship(BekpackUser, back_populates=BekpackUser.owned_bags)
-    owner_trip_id = Column(Integer, ForeignKey(BekpackTrip.id), index=True)
-    owner_trip = relationship(BekpackTrip, back_populates=BekpackTrip.bags)
-
     color = Column(ColorType)
-    name = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
     items = relationship("BekpackListItem", back_populates="owner")
+    name = Column(String)
+    owner = relationship(BekpackUser, back_populates=BekpackUser.owned_bags)
+    owner_id = Column(Integer, ForeignKey(BekpackUser.id), index=True)
+    owner_trip = relationship(BekpackTrip, back_populates=BekpackTrip.bags)
+    owner_trip_id = Column(Integer, ForeignKey(BekpackTrip.id), index=True)
 
 
 class BekpackListItem(Base):
-    id = Column(Integer, primary_key=True, index=True)
-    bag_id = Column(Integer, ForeignKey(BekpackBag.id))
     bag = relationship(BekpackBag, back_populates=BekpackBag.items)
-    parent_list = Column(Integer, ForeignKey(BekpackTripItemList.id))
-    list_index = Column(Integer)
-    quantity = Column(Integer)
-    name = Column(String)
+    bag_id = Column(Integer, ForeignKey(BekpackBag.id))
     description = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    list_index = Column(Integer)
+    name = Column(String)
+    parent_list_id = Column(
+        Integer, ForeignKey(BekpackTripItemList.id), primary_key=True, index=True
+    )
+    quantity = Column(Integer)
