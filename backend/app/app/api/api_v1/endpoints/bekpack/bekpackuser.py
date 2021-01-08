823db2e295f56b98db1c2a98177bd5107a94211f
Christopher Chen <christopher.chen1995@gmail.com>
2021-01-08 14:54:27 -0800

    Reorder API endpoints to be more semantic

--- backend/app/app/api/api_v1/endpoints/bekpack/bekpackuser.py
+++ backend/app/app/api/api_v1/endpoints/bekpack/bekpackuser.py
@@ -15,6 +15,23 @@ from app.api import deps
 router = APIRouter()
 
 
+@router.post("/", response_model=schemas.BekPackUser)
+def create_bekpackuser(
+    *,
+    db: Session = Depends(deps.get_db),
+    user_in: schemas.BekPackUserCreate,
+    current_user: User = Depends(deps.get_current_active_user),
+) -> Any:
+    """Create new BekpackUser"""
+    try:
+        bp_user = crud_bekpackuser.create_with_owner(
+            db=db, obj_in=user_in, owner_id=current_user.id
+        )
+    except sqlalchemy.exc.IntegrityError:
+        raise HTTPException(status_code=404, detail="BekpackUser already exists")
+    return bp_user
+
+
 @router.get("/me", response_model=schemas.BekPackUser)
 def get_bekpackuser_me(
     *,
@@ -32,6 +49,27 @@ def get_bekpackuser_me(
         current_user.id != bp_user.owner_id
     ):
         raise HTTPException(status_code=400, detail="Not enough permissions")
+    return bp_user
+
+
+@router.delete("/me", response_model=schemas.BekPackUser)
+def delete_bekpackuser_me(
+    *,
+    db: Session = Depends(deps.get_db),
+    current_user: User = Depends(deps.get_current_active_user),
+) -> Any:
+    """Delete a BekpackUser"""
+    try:
+        bp_user = crud_bekpackuser.get_by_owner(db=db, owner_id=current_user.id)
+    except sqlalchemy.orm.exc.NoResultFound:
+        raise HTTPException(status_code=404, detail="BekpackUser not found")
+    if not bp_user:
+        raise HTTPException(status_code=404, detail="BekpackUser not found")
+    if not crud_user.is_superuser(current_user) and (
+        current_user.id != bp_user.owner_id
+    ):
+        raise HTTPException(status_code=400, detail="Not enough permissions")
+    bp_user = crud_bekpackuser.remove(db=db, id=bp_user.id)
     return bp_user
 
 
@@ -93,23 +131,6 @@ def get_bekpackuser_by_id(
     return bp_user
 
 
-@router.post("/", response_model=schemas.BekPackUser)
-def create_bekpackuser(
-    *,
-    db: Session = Depends(deps.get_db),
-    user_in: schemas.BekPackUserCreate,
-    current_user: User = Depends(deps.get_current_active_user),
-) -> Any:
-    """Create new BekpackUser"""
-    try:
-        bp_user = crud_bekpackuser.create_with_owner(
-            db=db, obj_in=user_in, owner_id=current_user.id
-        )
-    except sqlalchemy.exc.IntegrityError:
-        raise HTTPException(status_code=404, detail="BekpackUser already exists")
-    return bp_user
-
-
 @router.delete("/{id}", response_model=schemas.BekPackUser)
 def delete_bekpackuser(
     *,
@@ -126,25 +147,4 @@ def delete_bekpackuser(
     ):
         raise HTTPException(status_code=400, detail="Not enough permissions")
     bp_user = crud_bekpackuser.remove(db=db, id=id)
-    return bp_user
-
-
-@router.delete("/me", response_model=schemas.BekPackUser)
-def delete_bekpackuser_me(
-    *,
-    db: Session = Depends(deps.get_db),
-    current_user: User = Depends(deps.get_current_active_user),
-) -> Any:
-    """Delete a BekpackUser"""
-    try:
-        bp_user = crud_bekpackuser.get_by_owner(db=db, owner_id=current_user.id)
-    except sqlalchemy.orm.exc.NoResultFound:
-        raise HTTPException(status_code=404, detail="BekpackUser not found")
-    if not bp_user:
-        raise HTTPException(status_code=404, detail="BekpackUser not found")
-    if not crud_user.is_superuser(current_user) and (
-        current_user.id != bp_user.owner_id
-    ):
-        raise HTTPException(status_code=400, detail="Not enough permissions")
-    bp_user = crud_bekpackuser.remove(db=db, id=bp_user.id)
     return bp_user
