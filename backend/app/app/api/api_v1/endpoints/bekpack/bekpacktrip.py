e95bd2e84320d9930ce951ff0b007afe76436c8e
Christopher Chen <christopher.chen1995@gmail.com>
2021-03-18 11:04:32 -0700

    Clean up imports

--- backend/app/app/api/api_v1/endpoints/bekpack/bekpacktrip.py
+++ backend/app/app/api/api_v1/endpoints/bekpack/bekpacktrip.py
@@ -1,20 +1,16 @@
 from typing import Any, List
-from app.api.api_v1 import DefaultCrudRouter
-from app.crud.crud_bekpack.crud_bekpacktrip import CRUDBekpackTrip
 
-import sqlalchemy
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import Depends, HTTPException
 from fastapi_pagination import Page, pagination_params
 from fastapi_pagination.paginator import paginate
 from sqlalchemy.orm import Session
 
 import app.schemas as schemas
+from app import crud, models, schemas
 from app.api import deps
-from app.crud import bekpacktrip as crud_bekpacktrip
-from app.crud import bekpackitemlist as crud_bekpackitemlist
-from app.crud import bekpackuser as crud_bekpackuser
+from app.api.api_v1 import DefaultCrudRouter
+from app.crud.crud_bekpack.crud_bekpacktrip import CRUDBekpackTrip
 from app.models import User
-from app import models, crud, schemas
 
 router = DefaultCrudRouter[
     models.BekpackTrip, CRUDBekpackTrip, schemas.BekpackTrip, schemas.BekpackTripUpdate
@@ -36,7 +32,7 @@ def get_my_bakpacktrips(
     db: Session = Depends(deps.get_db),
     current_user: User = Depends(deps.get_current_active_user),
 ) -> Page[schemas.BekpackTrip]:
-    records = crud_bekpacktrip.get_by_owner(db=db, owner_id=current_user.id)
+    records = crud.bekpacktrip.get_by_owner(db=db, owner_id=current_user.id)
     return paginate(records)
 
 
@@ -50,11 +46,11 @@ def get_bekpacktrip_lists(
     db: Session = Depends(deps.get_db),
     current_user: User = Depends(deps.get_current_active_user),
     trip_id: int,
-) -> List[schemas.BekpackTrip]:
-    if not crud_bekpacktrip.user_can_read(db=db, object=id, user=current_user):
+) -> Page[schemas.BekpackTrip]:
+    if not crud.bekpacktrip.user_can_read(db=db, object=id, user=current_user):
         raise HTTPException(status_code=403, detail="Not enough permissions")
 
-    records = crud_bekpackitemlist.get_by_trip(db=db, trip_id=trip_id)
+    records = crud.bekpackitemlist.get_by_trip(db=db, trip_id=trip_id)
     return paginate(records)
 
 
@@ -67,6 +63,6 @@ def create_bekpacktrip(
 ) -> Any:
     """Create new BekpackTrip"""
 
-    trip = crud_bekpacktrip.create_with_owner(db=db, obj_in=trip_in, owner=current_user)
+    trip = crud.bekpacktrip.create_with_owner(db=db, obj_in=trip_in, owner=current_user)
 
     return trip
