5bed4a1ea97744ff76a56c2a613da0c8f3bfc4df
Christopher Chen <christopher.chen1995@gmail.com>
2021-09-17 18:13:33 -0700

    Implement some tests for BekpackItemListItem

--- backend/app/app/tests/crud/bekpack/conftest.py
+++ backend/app/app/tests/crud/bekpack/conftest.py
@@ -3,12 +3,13 @@ import pytest
 from sqlalchemy.orm.session import Session
 
 from app.models.bekpack import BekpackUser
+from app.models.user import User
 from app.tests.utils.bekpack import create_random_bekpackuser
 from app import crud
 
 
 @pytest.fixture(scope="function")
-def user_registered_random(db: Session) -> Generator[BekpackUser, None, None]:
+def user_registered_random(db: Session) -> Generator[User, None, None]:
     bpuser = create_random_bekpackuser(db)
     yield bpuser.owner
     crud.bekpackuser.remove(db=db, id=bpuser.id)
@@ -16,7 +17,7 @@ def user_registered_random(db: Session) -> Generator[BekpackUser, None, None]:
 
 
 @pytest.fixture(scope="module")
-def user_registered_constant(db: Session) -> Generator[BekpackUser, None, None]:
+def user_registered_constant(db: Session) -> Generator[User, None, None]:
     bpuser = create_random_bekpackuser(db)
     yield bpuser.owner
     crud.bekpackuser.remove(db=db, id=bpuser.id)
--- backend/app/app/tests/crud/bekpack/test_crud_bekpackitemlistitem.py
+++ backend/app/app/tests/crud/bekpack/test_crud_bekpackitemlistitem.py
@@ -30,3 +30,40 @@ def test_create_bekpackitemlistitem_unauthorized(
             parent_itemlist_id=itemlist.id,
             user=user_registered_random,
         )
+
+
+def test_create_bekpackitemlistitem(
+    db: Session, user_registered_random: models.User
+) -> None:
+    itemlist = create_random_itemlist(db)
+    owner = itemlist.parent_trip.owner.owner
+    obj = crud.bekpackitemlistitem.create_with_itemlist(
+        db=db,
+        obj_in=BekpackItemListItemCreate(
+            description=random_lower_string(), name=random_lower_string(), quantity=1
+        ),
+        parent_itemlist_id=itemlist.id,
+        user=owner,
+    )
+    assert obj
+    assert obj.parent_list_id == itemlist.id
+    assert obj.bag_id == None
+
+
+def test_read_bekpackitemlistitem_unauthorized(
+    db: Session, user_registered_random: models.User
+) -> None:
+    itemlist = create_random_itemlist(db)
+    owner = itemlist.parent_trip.owner.owner
+    obj = crud.bekpackitemlistitem.create_with_itemlist(
+        db=db,
+        obj_in=BekpackItemListItemCreate(
+            description=random_lower_string(), name=random_lower_string(), quantity=1
+        ),
+        parent_itemlist_id=itemlist.id,
+        user=owner,
+    )
+    can = crud.bekpackitemlistitem.user_can_read(
+        db=db, id=obj.id, user=user_registered_random
+    )
+    assert not can
